ab9ba2bb1afe61e2b9a15afac2f0b9fb2eed984b
Matthew Smith <matthew.smith813@gmail.com>
2015-12-14 23:26:01 +0000

    adding some extra creeps

--- game/dota_addons/berrytd/scripts/vscripts/waves.lua
+++ game/dota_addons/berrytd/scripts/vscripts/waves.lua
@@ -1,11 +1,10 @@
-function Wave(creep, numTotal, goldPerKill, bonusEndGold)
+function Wave(creep, numTotal, bonusEndGold)
 	local newWave = {}
 	newWave.isBoss = false
 	newWave.creep = creep
 	newWave.numTotal = numTotal
 	newWave.numPerSpawn = numPerSpawn
-	newWave.bonusEndGold = bonusEndGold
-	newWave.goldPerKill = goldPerKill
+	newWave.bonusEndGold = bonusEndGold	
 	newWave.lifePenalty = 1
 
 	return newWave
@@ -13,5 +12,7 @@ end
 
 waveTable = {}
 
-waveTable[1] = Wave("creep_sheep", 10, 5, 100)
-waveTable[2] = Wave("creep_cluckles", 10, 10, 100)
+waveTable[1] = Wave("creep_sheep", 10, 100)
+waveTable[2] = Wave("creep_cluckles", 10, 100)
+waveTable[3] = Wave("creep_donkey", 10, 100)
+waveTable[4] = Wave("creep_beaver", 10, 100)
